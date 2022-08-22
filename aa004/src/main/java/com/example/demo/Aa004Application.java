3154912489600b476c5ba0de87b1084f27daeb2b
ouzza <whdgns955@naver.com>
2022-08-22 10:00:12 +0900

    프로젝트 최초 강동원

---
+++ aa004/src/main/java/com/example/demo/Aa004Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa004Application.class, args);
+	}
+
+}
